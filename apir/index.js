50e2fb496713cd4580503f01987cc898d0068932
Darko <darevski1@gmail.com>
2020-12-05 12:02:43 +0100

    Day 1

---
+++ apir/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cars = require('./handlers/books');
+const api = express();
+
+
+
+api.use(bodyParser.json());
+
+
+api.get("/cars", cars.getAll);
+api.get('/cars/:id', cars.getOne);
+api.post('/cars/', cars.save);
+api.put('/cars/:id', cars.update);
+api.patch('cars/:id', cars.updatePartials);
+api.delete('/cars/:id', cars.remove);
+
+
+
+api.listen(9000, err => {
+    if (err) {
+        console.log(err);
+    }
+    console.log('Server stared on port 9000');
+})
+
+
